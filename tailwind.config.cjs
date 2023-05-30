13c7a7529878fcfcdd7f4d2c318bbe7defc8cfab
Thomas Findlay <thomasfindlay94@gmail.com>
2023-05-30 15:34:24 +0100

    remove tailwind colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,12 @@
 // eslint-disable-next-line
 const colors = require('tailwindcss/colors')
 
+delete colors['lightBlue']
+delete colors['warmGray']
+delete colors['trueGray']
+delete colors['coolGray']
+delete colors['blueGray']
+
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
