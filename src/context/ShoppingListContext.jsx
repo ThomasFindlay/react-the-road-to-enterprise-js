004aea7fee44098dcdcc69c4a5e616572ab64137
Thomas Findlay <thomasfindlay94@gmail.com>
2023-08-25 10:54:35 +0100

    cleanup

--- src/context/ShoppingListContext.jsx
+++ src/context/ShoppingListContext.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { useImmerReducer } from 'use-immer'
-import { contextFactory } from './helpers/contextFactory'
-import propTypes from 'prop-types'
+import React from 'react';
+import { useImmerReducer } from 'use-immer';
+import { contextFactory } from './helpers/contextFactory';
+import propTypes from 'prop-types';
 
 const [
   ShoppingListContext,
   useShoppingListContext,
   useShoppingListContextSelector,
-] = contextFactory()
+] = contextFactory();
 
-export { useShoppingListContext, useShoppingListContextSelector }
+export { useShoppingListContext, useShoppingListContextSelector };
 
-const shoppingItems  = {
+const shoppingItems = {
   newShoppingItemName: '',
   items: [
     {
@@ -27,42 +27,39 @@ const shoppingItems  = {
       name: 'Chicken breasts',
     },
   ],
-}
+};
 
-const reducer = (
-  state,
-  action
-) => {
+const reducer = (state, action) => {
   switch (action.type) {
     case 'UPDATE_NEW_SHOPPING_ITEM_NAME':
-      state.newShoppingItemName = action.payload
-      break
+      state.newShoppingItemName = action.payload;
+      break;
     case 'ADD_ITEM':
-      state.newShoppingItemName = ''
-      state.items.push(action.payload)
-      break
+      state.newShoppingItemName = '';
+      state.items.push(action.payload);
+      break;
     case 'UPDATE_ITEM':
-      state.items.splice(action.payload.index, 1, action.payload.item)
-      break
+      state.items.splice(action.payload.index, 1, action.payload.item);
+      break;
     case 'DELETE_ITEM':
-      state.items.splice(action.payload.index, 1)
-      break
+      state.items.splice(action.payload.index, 1);
+      break;
   }
-  return state
-}
+  return state;
+};
 
 const ShoppingListContextProvider = (props) => {
-  const [shoppingList, dispatch] = useImmerReducer(reducer, shoppingItems)
+  const [shoppingList, dispatch] = useImmerReducer(reducer, shoppingItems);
 
   return (
     <ShoppingListContext.Provider value={[shoppingList, dispatch]}>
       {props.children}
     </ShoppingListContext.Provider>
-  )
-}
+  );
+};
 
 ShoppingListContextProvider.propTypes = {
-  children: propTypes.children
-}
+  children: propTypes.children.isRequired,
+};
 
-export default ShoppingListContextProvider
+export default ShoppingListContextProvider;
