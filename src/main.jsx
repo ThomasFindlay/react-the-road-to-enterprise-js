b3ff552eff7247c5038fd10a3fa21d7527ceddd7
Thomas Findlay <thomasfindlay94@gmail.com>
2023-08-25 16:19:00 +0100

    Removed duplicate import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
 import './index.css';
 import App from './App.jsx';
 import { persistor, store } from './store';
