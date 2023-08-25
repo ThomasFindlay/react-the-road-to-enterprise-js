67ce58aca6c0dc785d2bf21c9d84b38d389a916e
Thomas <thomasfindlay94@gmail.com>
2023-08-25 14:15:35 +0100

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -13,7 +13,7 @@ const axiosInstance = axios.create(axiosParams)
 const api = (axios) => {
   return {
     get: (url, config = {}) => axios.get(url, config),
-    delete: (url, config = {}) => axios.get(url, config),
+    delete: (url, config = {}) => axios.delete(url, config),
     post: (url, body, config = {}) => axios.post(url, body, config),
     patch: (url, body, config = {}) => axios.post(url, body, config),
     put: (url, body, config = {}) => axios.post(url, body, config),
