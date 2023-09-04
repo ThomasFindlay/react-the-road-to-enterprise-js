73e2ef21021aea7167e4ae4994f1da4fb76c8cb3
Thomas Findlay <thomasfindlay94@gmail.com>
2023-09-04 16:46:04 +0100

    Migrate to Vite JS

---
+++ src/components/UsersManager/UsersManager.jsx
@@ -0,0 +1,36 @@
+import Spinner from '../Spinner';
+import AddUsers from './components/AddUsers';
+import DisplayUsers from './components/DisplayUsers';
+import SelectedUserDetails from './components/SelectedUserDetails';
+import { useFetchUsersQuery } from './usersSlice';
+
+const UsersManager = (props) => {
+  const {
+    data: users,
+    isError: isFetchUsersError,
+    isLoading: isFetchUsersPending,
+    isSuccess: isFetchUsersSuccess,
+  } = useFetchUsersQuery();
+
+  return (
+    <div className='container py-8 mx-auto'>
+      {isFetchUsersPending ? <Spinner show /> : null}
+      {isFetchUsersSuccess && users?.length ? (
+        <div className='grid grid-cols-12 gap-4 px-4'>
+          <div className='col-span-4'>
+            <AddUsers />
+          </div>
+          <div className='col-span-4'>
+            <DisplayUsers />
+          </div>
+          <div className='col-span-4'>
+            <SelectedUserDetails />
+          </div>
+        </div>
+      ) : null}
+      {isFetchUsersError ? <p>There was a problem fetching users</p> : null}
+    </div>
+  );
+};
+
+export default UsersManager;
--- src/components/UsersManager/components/DisplayUsers.jsx
+++ src/components/UsersManager/components/DisplayUsers.jsx
@@ -1,50 +1,47 @@
-import Spinner from '@/components/Spinner'
-import { useAppDispatch, useAppSelector } from '@/store/hooks'
+import Spinner from '@/components/Spinner';
 import {
   selectUser,
   useFetchUsersQuery,
   useRemoveUserMutation,
-} from '../usersSlice'
+} from '../usersSlice';
 
-type DisplayUsersProps = {}
-
-const DisplayUsers = (props: DisplayUsersProps) => {
-  const dispatch = useAppDispatch()
-  const deletingUserId = useAppSelector((state) => state.users.deletingUserId)
+const DisplayUsers = (props) => {
+  const dispatch = useDispatch();
+  const deletingUserId = useSelector((state) => state.users.deletingUserId);
   const {
     data: users,
     isSuccess: isFetchUsersSuccess,
     isLoading: isLoadingUsers,
-  } = useFetchUsersQuery()
+  } = useFetchUsersQuery();
   const [removeUser, { isLoading: isRemoveUserPending }] =
-    useRemoveUserMutation()
+    useRemoveUserMutation();
   return (
     <div>
-      <h2 className="font-semibold text-xl mb-4">Users</h2>
-      <ul className="space-y-3">
+      <h2 className='font-semibold text-xl mb-4'>Users</h2>
+      <ul className='space-y-3'>
         {isFetchUsersSuccess && Array.isArray(users)
           ? users.map((user) => {
               return (
-                <li key={user.id} className="space-x-3">
+                <li key={user.id} className='space-x-3'>
                   <button
-                    className="hover:underline"
+                    className='hover:underline'
                     onClick={() => dispatch(selectUser(user.id))}
                   >
                     {user.email}
                   </button>
                   {isRemoveUserPending && deletingUserId === user.id ? (
-                    <Spinner show size="sm" />
+                    <Spinner show size='sm' />
                   ) : (
                     <button onClick={() => removeUser(user)}>X</button>
                   )}
                 </li>
-              )
+              );
             })
           : null}
       </ul>
       {isLoadingUsers ? <Spinner show /> : null}
     </div>
-  )
-}
+  );
+};
 
-export default DisplayUsers
+export default DisplayUsers;
