e72547f8222a7435f38a506837de99b232280902
Thomas Findlay <thomasfindlay94@gmail.com>
2023-09-06 15:09:32 +0100

    Migrate to Vite JS

---
+++ src/components/EventsManager/components/DisplayEvents.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import EventsTabs from './EventsTabs';
+
+const DisplayEvents = (props) => {
+  const [eventsToShow, setEventsToShow] = useState('all');
+  const allEvents = [];
+  const selectEvent = (id) => {};
+
+  const { upcomingEvents, pastEvents } = {
+    upcomingEvents: [],
+    pastEvents: [],
+  };
+
+  const eventsMap = {
+    all: allEvents,
+    upcoming: upcomingEvents,
+    past: pastEvents,
+  };
+
+  const events = eventsMap[eventsToShow];
+
+  return (
+    <div>
+      <h2 className='font-semibold text-xl mb-6'>Events</h2>
+      <EventsTabs activeTab={eventsToShow} setActiveTab={setEventsToShow} />
+      <div className='mt-4'>
+        <ul className='text-left shadow py-4 space-y-3 divide-y'>
+          {Array.isArray(events) && events.length ? (
+            events.map((event) => {
+              return (
+                <li key={event.id} className='-mt-3'>
+                  <button
+                    className='hover:underline pt-3 px-4'
+                    onClick={() => selectEvent(event.id)}
+                  >
+                    {event.title} - {event.startDate}
+                  </button>
+                </li>
+              );
+            })
+          ) : (
+            <p className='mx-4'>No events</p>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default DisplayEvents;
--- src/components/EventsManager/eventsTypes.ts
+++
@@ -1,8 +0,0 @@
-export type Event = {
-  id: string
-  title: string
-  startDate: string
-  startTime: string
-  endDate: string
-  endTime: string
-}
