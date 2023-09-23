bf7725a45740808b17f64e881e88f89501d3cb51
Thomas Findlay <thomasfindlay94@gmail.com>
2023-09-23 12:24:03 +0100

    Cleanup. Added jotai-immer

--- src/api/api.js
+++ src/api/api.js
@@ -1,28 +1,66 @@
-import axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
+import axios from 'axios';
+
 // Default config for the axios instance
 const axiosParams = {
   // Set different base URL based on the environment
-  baseURL:
-    process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : '/',
-}
+  baseURL: import.meta.env.DEV ? 'http://localhost:4000/api' : '/api',
+};
 
 // Create axios instance with default params
-const axiosInstance = axios.create(axiosParams)
+const axiosInstance = axios.create(axiosParams);
+
+export const didAbort = (error) => axios.isCancel(error);
+
+export const isApiError = (error) => {
+  return axios.isAxiosError(error);
+};
+
+const withAbort = (fn) => {
+  const executor = async (...args) => {
+    const originalConfig = args[args.length - 1];
+    // Extract abort property from the config
+    const { abort: setAbortFn, ...config } = originalConfig;
+
+    // Create cancel token and abort method only if abort
+    // function was passed
+    if (typeof setAbortFn === 'function') {
+      const abortController = new AbortController();
+      config.signal = abortController.signal;
+      setAbortFn(() => {
+        abortController.abort();
+      });
+    }
+
+    try {
+      if (args.length > 2) {
+        const [url, body] = args;
+        return await fn(url, body, config);
+      } else {
+        const [url] = args;
+        return await fn(url, config);
+      }
+    } catch (error) {
+      // Add "aborted" property to the error if the request was cancelled
+      if (didAbort(error)) {
+        error.aborted = true;
+      }
+
+      throw error;
+    }
+  };
+
+  return executor;
+};
 
 // Main api function
-const api = (axios: AxiosInstance) => {
+const api = (axios) => {
   return {
-    get: <T>(url: string, config: AxiosRequestConfig = {}) =>
-      axios.get<T>(url, config),
-    delete: <T>(url: string, config: AxiosRequestConfig = {}) =>
-      axios.get<T>(url, config),
-    post: <T>(url: string, body: unknown, config: AxiosRequestConfig = {}) =>
-      axios.post<T>(url, body, config),
-    patch: <T>(url: string, body: unknown, config: AxiosRequestConfig = {}) =>
-      axios.post<T>(url, body, config),
-    put: <T>(url: string, body: unknown, config: AxiosRequestConfig = {}) =>
-      axios.post<T>(url, body, config),
-  }
-}
+    get: (url, config = {}) => withAbort(axios.get)(url, config),
+    delete: (url, config = {}) => withAbort(axios.delete)(url, config),
+    post: (url, body, config = {}) => withAbort(axios.post)(url, body, config),
+    patch: (url, body, config = {}) => withAbort(axios.post)(url, body, config),
+    put: (url, body, config = {}) => withAbort(axios.post)(url, body, config),
+  };
+};
 
-export default api(axiosInstance)
+export default api(axiosInstance);
--- src/helpers/pick.js
+++
@@ -1,8 +0,0 @@
-export const pick = (obj, ...keys) => {
-  let picked = {};
-  for (const key of keys) {
-    picked[key] = obj[key];
-  }
-
-  return picked;
-};
