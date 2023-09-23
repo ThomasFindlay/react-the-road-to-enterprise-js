b9525e067a9cb55300d22186573f0b35faff30d6
Thomas Findlay <thomasfindlay94@gmail.com>
2023-09-23 22:12:19 +0100

    Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import EventsManager from './components/EventsManager/EventsManager'
-import './App.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import EventsManager from './components/EventsManager/EventsManager';
+import './App.css';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="App mx-auto max-w-6xl text-center my-8">
-        <h1 className="font-semibold text-2xl">
+      <div className='App mx-auto max-w-6xl text-center my-8'>
+        <h1 className='font-semibold text-2xl'>
           React - The Road To Enterprise
         </h1>
 
         <EventsManager />
       </div>
     </QueryClientProvider>
-  )
+  );
 }
 
-export default App
+export default App;
