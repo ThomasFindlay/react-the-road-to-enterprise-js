600afe6486f477bad6035ac791c8399af123707b
Thomas Findlay <thomasfindlay94@gmail.com>
2023-09-25 10:52:38 +0100

    Migrate to JS

---
+++ src/components/Search.jsx
@@ -0,0 +1,38 @@
+import { fetchMeals } from '@/api/meal.api';
+import React, { useState } from 'react';
+
+const Search = (props) => {
+  const [query, setQuery] = useState('');
+  const [meals, setMeals] = useState([]);
+
+  const initSearchApiRequest = async (q) => {
+    setMeals(await fetchMeals(q));
+  };
+
+  const onChangeQuery = (e) => {
+    const q = e.target.value;
+    setQuery(q);
+    initSearchApiRequest(q);
+  };
+
+  return (
+    <div className='space-y-6'>
+      <form className='flex flex-col items-start mx-auto max-w-[20rem]'>
+        <label>Search meals</label>
+        <input
+          className='w-full'
+          type='text'
+          value={query}
+          onChange={onChangeQuery}
+        />
+      </form>
+      <ul>
+        {meals.map((meal) => {
+          return <li key={meal.idMeal}>{meal.strMeal}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Search;
