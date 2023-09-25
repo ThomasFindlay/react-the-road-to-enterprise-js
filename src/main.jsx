88fbdd587240ba976d0fedd0c1ebb571add870eb
Thomas Findlay <thomasfindlay94@gmail.com>
2023-09-25 12:29:23 +0100

    Migrate to JS

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles/base.css';
 import './index.css';
 
 /* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
